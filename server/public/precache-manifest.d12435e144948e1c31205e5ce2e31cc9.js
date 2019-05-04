90e8a895a3a5d1941dd1994432eed7a37de58148
Guilherme Canton <guilcanton@gmail.com>
2019-05-04 10:23:11 -0300

    arquivos pwa adicionados ao projeto(serviceWorker e manifest.json)

---
+++ server/public/precache-manifest.d12435e144948e1c31205e5ce2e31cc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "07e2fd9c4648411e9d5d",
+    "url": "/js/chunk-vendors.efeccc54.js"
+  },
+  {
+    "revision": "26e8c83a2fb3e78b90d8",
+    "url": "/js/app.0acd5920.js"
+  },
+  {
+    "revision": "31b95eef99f43058dc8083c7e7588cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e8c83a2fb3e78b90d8",
+    "url": "/css/app.61cb56a2.css"
+  }
+];
